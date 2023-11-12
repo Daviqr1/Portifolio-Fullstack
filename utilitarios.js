2d19b2e0d83dadb01349a815e0f99bd38a775b88
Davi Barros de Rezende <84293017+Daviqr1@users.noreply.github.com>
2023-11-12 02:55:45 -0300

    Add files via upload

---
+++ utilitarios.js
@@ -0,0 +1,13 @@
+const perfilInstagram = "https://www.instagram.com/davi_b.rezende/";
+
+const redirecionarParaInstagram = () => {
+  window.open(perfilInstagram, "_blank");
+};
+const PerfilLinkedin = "https://www.linkedin.com/in/davi-barros-de-rezende-09540b222/";
+const redirecionarParaLinkedin = () => {
+  window.open(PerfilLinkedin, "_blank");
+}
+const PerfilGithub = "https://github.com/Daviqr1"
+const redirecionarParaGithub = () => {
+  window.open(PerfilGithub, "_blank");
+}
